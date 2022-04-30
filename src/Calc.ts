e7a7a83122644bb9163245f6dd21f3a0ca9b31d6
Josep Servat <jservat@outlook.es>
2022-05-01 00:12:50 +0200

    Add ts support , eslint , husky

---
+++ src/Calc.ts
@@ -0,0 +1,12 @@
+class Calc {
+  add(a: number, b: number) : number {
+    return a + b
+  }
+}
+
+
+export default Calc;
+
+// const newCalc = new Calc();
+
+// console.log(newCalc.add(231, 23))
---
+++ src/calc.test.ts
@@ -0,0 +1,13 @@
+import Calc from '../src/Calc';
+
+describe('Calc', () => {
+  test('should return 10 for ', () => {
+    const calc = new Calc();
+    expect(calc.add(6, 4)).toBe(10)
+  })
+
+  test('shoud return 9 for add(10, -1)', () => {
+    const calc = new Calc();
+    expect(calc.add(10, -1)).toBe(9)
+  })
+})
