ddc62f6ca24d02f7fc0f66a4c9e8227c1070dff8
Josep Servat <jservat@outlook.es>
2022-05-04 01:25:03 +0200

    return most profit

--- __tests__/mostProfit.test.ts
+++ __tests__/mostProfit.test.ts
@@ -1,4 +1,4 @@
-import { getMostProfitFromStockQuotes } from '../challenges/mostProfit'
+import { getMostProfitFromStockQuotes, getMostProfitCodeWars, getMostProfitReduce } from '../challenges/mostProfit'
 
 describe("Most Profit", function() {
   it("should return most profit", function() {
@@ -8,5 +8,24 @@ describe("Most Profit", function() {
 
   it("should get no profit", () => {
     expect(getMostProfitFromStockQuotes([7,6,4,3,1])).toBe(0);
+  })
+})
+
+
+describe("Most Profit codewars", function() {
+  it("should return most profit", function() {
+    expect(getMostProfitCodeWars([1,2,3,4,5,6])).toBe(15);
+    expect(getMostProfitCodeWars([6,5,4,3,2,1])).toBe(0);
+    expect(getMostProfitCodeWars([1,6,5,10,8,7])).toBe(18);
+    expect(getMostProfitCodeWars([1,2,10,3,2,7,3,2])).toBe(26);
+  })
+})
+
+describe("Most Profit codewars", function() {
+  it("should return most profit", function() {
+    expect(getMostProfitReduce([1,2,3,4,5,6])).toBe(15);
+    expect(getMostProfitReduce([6,5,4,3,2,1])).toBe(0);
+    expect(getMostProfitReduce([1,6,5,10,8,7])).toBe(18);
+    expect(getMostProfitReduce([1,2,10,3,2,7,3,2])).toBe(26);
   })
 })
